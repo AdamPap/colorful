711fd0c5af8d02a125f4755f1eece3f556c1dd50
AdamPap <adampapamast@hotmail.com>
2021-07-18 23:43:10 +0300

    fixed SSR issues with next and mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 // import '../styles/globals.css'
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
 export default MyApp;
